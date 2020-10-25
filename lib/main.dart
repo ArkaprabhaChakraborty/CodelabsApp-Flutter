3d50184b4ec506d680a3e362091c464737702635
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-10-25 13:28:18 +0530

    final for Write your first flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,34 @@ class _RandomWordsState extends State<RandomWords> {
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
   void _pushSaved() 
   {
-    
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
   }
   @override
   Widget build(BuildContext context) {
